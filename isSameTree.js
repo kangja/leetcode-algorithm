2c87840779648f7d908f5bccfa7b7d441878e6bc
Jason <kangja1996@gmail.com>
2021-10-16 23:11:03 -0400

    delete examples

--- isSameTree.js
+++ isSameTree.js
@@ -13,26 +13,3 @@ let isSameTree = function (p, q) {
 
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
-
-// example
-let p = [1, 2, 3];
-let q = [1, 2, 3];
-console.log(isSameTree(p, q));
-// Input: (p = [1, 2, 3]), (q = [1, 2, 3]);
-// Output: true;
-
-// example
-// let p = [1, 2];
-// let q = [1, null, 2];
-// console.log(isSameTree(p, q));
-
-// Input: (p = [1, 2]), (q = [1, null, 2]);
-// Output: false;
-
-// example
-// let p = [1, 2, 1];
-// let q = [1, 1, 2];
-// console.log(isSameTree(p, q));
-
-// Input: (p = [1, 2, 1]), (q = [1, 1, 2]);
-// Output: false;
